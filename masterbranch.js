29bae0b668b5b671a0764e0f4b4ca9add4ac4fcf
thealphapanda <50777862+thealphapanda@users.noreply.github.com>
2019-05-30 19:30:51 +0900

    Initial Commit
    Initialized Variables on JS File

---
+++ masterbranch.js
@@ -0,0 +1,5 @@
+var post_Header_Image = document.getElementById("post-header-image"),
+	post_Header_Name = document.getElementById("post-header-name"),
+	post_Header_Role = document.getElementById("post-header-role"),
+	post_Date_Time = document.getElementById("post-date-time"),
+	post_Content_Text = document.getElementById("post-content-text");
