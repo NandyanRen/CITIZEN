a92970b1aef8b0a633080bac37d5a3de8236a705
John Ren Veluz <johnrencveluz@gmail>
2019-07-18 21:47:48 +0800

    Working Python email fetch and added css, HTML structure

--- gmail/readEmail.py
+++ gmail/readEmail.py
@@ -1,15 +1,56 @@
+#!D:/Program Files/Python 2.7.16/python.exe
+
+print("Content-Type: text/html\r\n\r\n")
+print('''
+    <head>
+  <title>Home</title>
+  <link rel="stylesheet" type="text/css" href="gmail_python.css">
+</head>
+
+<body>
+  <!--<form action="readEmail.py" method="post">-->
+  <div class = "left-menu">
+    <h1 id = "left-menu-header">Groups</h1>
+      <div class = "left-menu-subcontainer">
+      <div>
+        <a href="../index.html">
+          <button id = "left-menu-content" type="button">Twitter</button>
+        </a>
+      </div>
+      <div>
+        <a href="index.html">
+          <button id = "left-menu-content" type="button">Gmail</button>
+        </a>
+      </div>
+      <div>
+        <a href="../facebook/index.html">
+          <button id = "left-menu-content" type="button">Facebook</button></div>
+        </a>
+      </div>
+      <div>
+        <a href="../canvas/index.html">
+        <button id = "left-menu-content" type="button">Canvas</button>
+        </a>
+      </div>
+    </div>
+  </div>
+
+  <div class = "middle-content">
+    <div class = "posts">
+      <div class = "post-header">
+      ''')
 
 import smtplib
 import time
 import imaplib
 import email
 import re
+import cgi
 
 # Gmail Credentials
 FROM_EMAIL = ''  # Enter your email here
 FROM_PWD = ''  # Enter your email password here
 SMTP_SERVER = 'imap.gmail.com'  # imap.gmail.com is the STMP server to fetch emails.
-
 
 def read_email_from_gmail():
     try:
@@ -33,8 +74,12 @@ def read_email_from_gmail():
                     email_subject = msg['subject'] # Gets the Email Subject
                     email_from = msg['from'] # Gets the Email Subject
                     body = ""  # Initialization of body string variable for the body of the message
-                    print('From : ' + email_from + '\n')
-                    print('Subject : ' + email_subject + '\n')
+
+                    # PRINTS RECIPIENT AND SUBJECT OF THE MESSAGE
+                    print('<div id = "post-header-from"><p>From : ' + email_from + '\n</p></div>')
+                    print('<div id = "post-header-subject"><p>Subject : ' + email_subject + '\n</p></div>')
+
+
                     for part in msg.walk():  # Since the body of the message is a multiLine and retuns a list instead of string. This msg.walk will decode the message.
                         if part.get_content_type() == "text/plain":
                             email_body = part.get_payload(decode=True)  # Decoder of the Message bytes to list
@@ -42,10 +87,13 @@ def read_email_from_gmail():
 
                     # body = body.strip('b"')
                     # body = body.strip('b\'')
-                    body = re.sub(r"^\S\"", " ", body)  # Removes the b" at the start of the String Message
-                    body = re.sub(r"^\S\'", " ", body)  # Removes the b' at the start of the String Message
-                    body = re.sub(r"\\r\\n", "<br>", body)  # Replaces all the \r\n to <br>
-                    print('Body : ' + body + '\n')  # Prints the body of the message
+                    # body = re.sub(r"^\S\"", " ", body)  # Removes the b" at the start of the String Message
+                    # body = re.sub(r"^\S\'", " ", body)  # Removes the b' at the start of the String Message
+                    # body = re.sub(r"\\r\\n", "<br>", body)  # Replaces all the \r\n to <br>
+                    body = re.sub(r"\n", "<br>", body)  # Replaces all the \r\n to <br>
+
+                    # PRINTS THE BODY OF THE MESSAGE
+                    print('<div id = "post-content"><p>' + body + '\n</p><hr>')  # Prints the body of the message
 
                     counter += 1
                     if counter == 5:
@@ -54,5 +102,15 @@ def read_email_from_gmail():
     except Exception as e:
         print(str(e))  # Prints an error is message is not successfully fetch.
 
-
 read_email_from_gmail()  # Calls the Method to read the messages of the email.
+
+print('''
+    </div>
+    </div>
+    </div>
+  </div>
+
+
+</body>
+  
+</html>''')
