c94174df47f9e03ec598e1e0e752324823747b4b
John Ren Veluz <johnrencveluz@gmail>
2019-08-08 04:46:58 +0800

    login page, redirect, youtube iframe, compressed images

--- login/login.js
+++ login/login.js
@@ -1,11 +1,5 @@
 function onSignIn(googleUser)
         {
           var profile=googleUser.getBasicProfile();
-          $(".g-signin2").css("display","none");
-          $("header").css("display","block");
-          $(".wrapper").css("display","block");
-          var vid = document.getElementById("videoPlayer");
-          vid.load();
-          vid.play();
-          setTimeout(function(){ document.getElementById('calendar-frame').location.reload(); }, 3000);
+          window.location.href='home/index.html';
         }
